940c30f22fca247ab2647aa50f445cc8de5ef14d
SK- KING <gan49ylf@yahoo.com>
2025-01-07 10:51:20 +0530

    Create Button.py
    Add button.. 🎃

---
+++ Button.py
@@ -0,0 +1,54 @@
+## First Row One Button
+
+reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Example 1',
+
+url='https://t.me/+ZxOh5cao8UUyOWFl')]])
+
+## First Row Three Buttons
+
+reply_markup = InlineKeyboardMarkup(
+
+[[InlineKeyboardButton('Example 1', url='https://t.me/SAKSHI_UPDATES'),
+
+InlineKeyboardButton('Example 2', url='https://t.me/SAKSHI_UPDATES'),
+
+InlineKeyboardButton('Example 3', url='https://t.me/SAKSHI_UPDATES')] ])
+
+⚠️ YOU can add more buttons or YOU can also remove the buttons that are in the
+
+middle
+
+## First & Second Both Row Two Buttons
+
+reply_markup = InlineKeyboardMarkup([
+
+[InlineKeyboardButton (' Example 1 ', url = 'http :// t.me /
+
+SAKSHI_UPDATES'), InlineKeyboardButton ('Example 2', url='http://t.me/
+
+SAKSHI_UPDATES')], [InlineKeyboardButton ('Example 3', url='http://t.me/
+
+SAKSHI_UPDATES'), InlineKeyboardButton ('Example 4', url='http://t.me/
+
+SAKSHI_UPDATES)] ])
+
+## First & Second Row Single Buttons
+
+reply_markup = InlineKeyboardMarkup([
+
+[InlineKeyboardButton (' Example 1 ', url = 'http :// t.me /
+
+SAKSHI_UPDATES')], [InlineKeyboardButton ('Example 2', url='http://t.me/
+
+SAKSHI_UPDATES')])]
+
+## First Row One, Second Row Two, Third Row One Buttons
+
+reply_markup = InlineKeyboardMarkup([
+[InlineKeyboardButton('Example 1', url='https://t.me/SAKSHI_UPDATES')], [
+
+InlineKeyboardButton('Example 2', url='https://t.me/SAKSHI_UPDATES'),
+
+InlineKeyboardButton('Example 3', url='https://t.me/SAKSHI_UPDATES')], [
+
+InlineKeyboardButton('Example 4', url='https://t.me/SAKSHI_UPDATES')] ])
